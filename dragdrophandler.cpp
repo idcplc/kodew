0b8a8d9e1f0ab11fb2144cc26f4eea3940192bc6
jzhan <kira.ost.08@gmail.com>
2017-09-28 20:26:27 +0800

    fix dragdrophandler

--- dragdrophandler.cpp
+++ dragdrophandler.cpp
@@ -2,6 +2,10 @@
 #include <QDebug>
 #include <fstream>
 #include <streambuf>
+#include <QFile>
+
+
+
 
 DragDropHandler::DragDropHandler(QObject *parent): QObject(parent)
 {
@@ -10,17 +14,32 @@ DragDropHandler::DragDropHandler(QObject *parent): QObject(parent)
 
 void DragDropHandler::runHandler(QString file)
 {
+    int substactor;
+#ifdef __APPLE__
+    substactor = 7;
+#elif _WIN32
+    substactor = 8;
+#elif __linux__
+    substactor = 7;
+#endif
+
     qDebug() << file << "\n";
     // ------------------------------------------------------------------------------------------------------
     // TODO: ini string nya kalau di mac dalam format "file://<path ke file>"
     // Untuk extrak <path ke file> pakai substr(7), menghilangkan 7 karakter di depan
     // Untuk windows (dan linux?) harus di cek dulu format string file supaya bisa di ekstrak path ke file nya
     // ------------------------------------------------------------------------------------------------------
-    std::string fpath = file.toStdString().substr(7);
+    /*std::string fpath = file.toStdString().substr(7);
    
     std::ifstream f(fpath);
     std::string fcontents((std::istreambuf_iterator<char>(f)),
-                          std::istreambuf_iterator<char>());
+                          std::istreambuf_iterator<char>());*/
+
+    QFile files(file.right(file.length() - substactor));
+
+    files.open(QIODevice::ReadOnly);
+
+    QTextStream in(&files);
     
     QObject *addSnippetView = this->parent()->findChild<QObject*>("addSnippetView");
     if(addSnippetView){
@@ -29,7 +48,7 @@ void DragDropHandler::runHandler(QString file)
         QObject *editSnippet = this->parent()->findChild<QObject*>("editSnippet");
         if(editSnippet){
             qDebug() << "editSnippet found" << "\n";
-            editSnippet->setProperty("text", fcontents.c_str());
+            editSnippet->setProperty("text", in.readAll());
         }
     }
 }
